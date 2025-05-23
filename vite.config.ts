49006f33835cac548976f469705fc858b58c11c4
mo-ibrahim22 <mibrahimm002@gmail.com>
2025-05-23 17:11:12 +0300

    Configured GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Document-Manager/',     
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
